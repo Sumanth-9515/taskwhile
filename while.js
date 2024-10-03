58497ff625580deb287f752f5c6bfc88c3242cfe
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2024-10-03 21:54:57 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,149 @@
+var a="tuesday"
+switch(a){
+    case "sunday":
+        console.log("this is sunday");
+        break
+        case "monday":
+            console.log("this is monday");
+            break
+            case "tuesday":
+                console.log("this is tuesday");
+                break
+                case "wednesday":
+                    console.log("this is wednesday");
+                    break
+                    case "thrusday":
+                        console.log("this is thrusday");
+                        break
+                        case "friday":
+                            console.log("this is friday");
+                            break
+                            default:
+                                console.log("none of the day");
+                                
+        
+}
+
+
+var b="august"
+switch(b){
+    case "january":
+        console.log("sankrathi is special in januaury");
+        break
+        case"february":
+        console.log("in feb it consists leap year");
+        break
+        case"march":
+        console.log("its march");
+        break
+        case"april":
+        console.log("its april");
+        break
+        case"may":
+        console.log("its may");
+        break
+        case"june":
+        console.log("its june");
+        break
+        case"july":
+        console.log("its july");
+        break
+        case"august":
+        console.log("its august");
+        break
+        case"september":
+        console.log("its september");
+        break
+        case"october":
+        console.log("its october");
+        break
+        default:
+            console.log("none of the month");
+        
+}
+
+
+a=1
+while(a<=10){
+    if(a%2==0){
+        console.log("its even number");
+    }else{
+        console.log("its odd number");
+        
+    }
+    a++
+      
+}
+i=10
+while(i>=1){
+    console.log("the game starts in"+i);
+    i--
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var a=1;
+// // while(a<=10){
+// //     console.log(a);
+// //     a++ 
+// // }
+
+// for(a=1;a<=20;a++){
+//     // console.log(a);
+//     if(a%2==0){
+//         console.log(a+" is even");
+// //     }  
+// }
+// for(a=1;a<=20;a++){
+//     if(a%2==5){
+//         console.log(a+"is odd");
+//     }
+// }
+
+
+// var a=0;
+// var limit=5
+// while(a<=limit){
+//     console.log("this atempt is " + a);
+//     a++;
+// }
+
+// for(a=1;a<=10;a++){
+//     console.log("this atempt is"+a);
+    
+// }
+
